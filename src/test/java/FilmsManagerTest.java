70a24fe445e96504d4fb82334aef73c5084cfe0d
KonstantinKhodot <Khodot.konstantin@mail.ru>
2022-10-11 14:11:03 +0300

    First commit

---
+++ src/test/java/FilmsManagerTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class FilmsManagerTest {
+
+    @Test
+    public void findLast() {
+        FilmsManager manager = new FilmsManager();
+        manager.addFilm("film1");
+        manager.addFilm("film2");
+        manager.addFilm("film3");
+        manager.addFilm("film4");
+        manager.addFilm("film5");
+        manager.addFilm("film6");
+        manager.addFilm("film7");
+        manager.addFilm("film8");
+        manager.addFilm("film9");
+        manager.addFilm("film10");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2", "film1"};
+
+        assertArrayEquals(expected, actual);
+    }
+}
