6bd6a67d5bc1dfcb4c404b4e33ac21b6aad28bf0
KonstantinKhodot <Khodot.konstantin@mail.ru>
2022-10-28 17:27:25 +0300

    Correction3

--- src/test/java/FilmsManagerTest.java
+++ src/test/java/FilmsManagerTest.java
@@ -25,6 +25,21 @@ public class FilmsManagerTest {
     }
 
     @Test
+    public void lessThanTheLimit() {
+        FilmsManager manager = new FilmsManager(5);
+        manager.addFilm("film1");
+        manager.addFilm("film2");
+        manager.addFilm("film3");
+        manager.addFilm("film4");
+        manager.addFilm("film5");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"film5", "film4", "film3", "film2", "film1"};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void moreThanTheLimit() {
         FilmsManager manager = new FilmsManager(11);
         manager.addFilm("film1");
@@ -58,30 +73,9 @@ public class FilmsManagerTest {
         manager.addFilm("film9");
         manager.addFilm("film10");
 
-        String[] actual = manager.findAll();
+        String[] actual = manager.findLast();
         String[] expected = {"film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2", "film1"};
 
         assertArrayEquals(expected, actual);
     }
-
-    @Test
-    public void lessThanTheLimit() {
-        FilmsManager manager = new FilmsManager(9);
-        manager.addFilm("film1");
-        manager.addFilm("film2");
-        manager.addFilm("film3");
-        manager.addFilm("film4");
-        manager.addFilm("film5");
-        manager.addFilm("film6");
-        manager.addFilm("film7");
-        manager.addFilm("film8");
-        manager.addFilm("film9");
-        manager.addFilm("film10");
-
-        String[] actual = manager.findAll();
-        String[] expected = {"film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2", "film1"};
-
-        assertArrayEquals(expected, actual);
-    }
-
 }
