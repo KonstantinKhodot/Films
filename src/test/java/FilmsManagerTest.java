0fa0ae5734bcdb5eba2bc9847168da32cb08b952
KonstantinKhodot <Khodot.konstantin@mail.ru>
2022-11-28 11:52:51 +0300

    correction10

--- src/test/java/FilmsManagerTest.java
+++ src/test/java/FilmsManagerTest.java
@@ -53,11 +53,12 @@ public class FilmsManagerTest {
         manager.addFilm("film9");
         manager.addFilm("film10");
         manager.addFilm("film11");
+        manager.addFilm("film12");
 
         String[] actual = manager.findLast();
-        String[] expected = {"film11", "film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2", "film1"};
+        String[] expected = {"film12", "film11", "film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2", "film1"};
 
-        assertArrayEquals(actual, expected);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
